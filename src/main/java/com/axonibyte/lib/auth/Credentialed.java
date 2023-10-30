e7bec2ba06869a66b8bcccc77c561be65605874b
Caleb L. Power <cpower@axonibyte.com>
2023-10-30 04:34:45 -0400

    implement session engine and model for credentialed users

---
+++ src/main/java/com/axonibyte/lib/auth/Credentialed.java
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2023 Axonibyte Innovations, LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.axonibyte.lib.auth;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.UUID;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.crypto.Signer;
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.bouncycastle.crypto.signers.Ed25519Signer;
+import org.bouncycastle.util.encoders.Base32;
+import org.bouncycastle.util.encoders.Base64;
+import org.bouncycastle.util.encoders.DecoderException;
+
+import dev.samstevens.totp.code.CodeGenerator;
+import dev.samstevens.totp.code.CodeVerifier;
+import dev.samstevens.totp.code.DefaultCodeGenerator;
+import dev.samstevens.totp.code.DefaultCodeVerifier;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.secret.SecretGenerator;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
+
+/**
+ * Represents a user or other entity that needs the ability to authenticate with
+ * the system.
+ *
+ * @author Caleb L. Power <cpower@axonibyte.com>
+ */
+public class Credentialed {
+  
+  private static byte[] globalMFASecret = new byte[32];
+
+  /**
+   * Sets the global secret used to encrypt MFA keys in the database.
+   *
+   * param secret the string representation of the MFA secret
+   */
+  public static void setGlobalMFASecret(String secret) {
+    byte[] buf = secret.getBytes();
+    for(int i = 0; i < (globalMFASecret.length > buf.length ? globalMFASecret.length : buf.length); i++)
+      globalMFASecret[i % globalMFASecret.length] ^= buf[i % buf.length];
+  }
+  
+  private UUID id = null;
+  private byte[] pubkey = null;
+  private byte[] mfakey = null;
+
+  /**
+   * Instantiates a credentialed user.
+   *
+   * @param id the user's unique identifier
+   * @param pubkey the user's public key
+   * @param mfakey the user's encrypted mfakey
+   */
+  public Credentialed(UUID id, byte[] pubkey, byte[] mfakey) {
+    this.id = id;
+    this.pubkey = pubkey;
+    this.mfakey = mfakey;
+  }
+
+  /**
+   * Retrieves the unique identifier associated with this user.
+   *
+   * @return the user's {@link UUID}
+   */
+  public UUID getID() {
+    return id;
+  }
+
+  /**
+   * Verifies a message and signature against this user's public key to ensure
+   * that this user is responsible for sending the message.
+   *
+   * @param message the message data itself
+   * @param sig the message signature
+   * @return true iff the signature is valid and verified
+   */
+  public boolean verifySig(String message, String sig) throws Exception {
+    byte[] msgBuf = message.getBytes();
+    byte[] sigBuf = Base64.decode(sig);
+    
+    Signer verifier = new Ed25519Signer();
+    verifier.init(false, new Ed25519PublicKeyParameters(this.pubkey));
+    verifier.update(msgBuf, 0, msgBuf.length);
+    return verifier.verifySignature(sigBuf);
+  }
+
+  /**
+   * Sets the public key associated with this user.
+   *
+   * @param pubkey the Base64 representation of the public key
+   */
+  public void setPubkey(String pubkey) {
+    this.pubkey = Base64.decode(pubkey);
+  }
+
+  /**
+   * Verifies a TOTP provided by the user.
+   *
+   * @return true if the TOTP is verified
+   */
+  public boolean verifyTOTP(String totp) {
+    if(null == this.mfakey && (null == totp || totp.isBlank())) return true;
+    if(null == this.mfakey) return false;
+    
+    try {
+      final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+      var key = new SecretKeySpec(globalMFASecret, "AES");
+      
+      ByteBuffer idBuf = ByteBuffer.wrap(new byte[16]);
+      idBuf.putLong(id.getMostSignificantBits());
+      idBuf.putLong(id.getLeastSignificantBits());
+      var iv = new IvParameterSpec(idBuf.array());
+      
+      cipher.init(Cipher.DECRYPT_MODE, key, iv);
+      var mfakey = new String(
+          Base32.encode(
+              cipher.doFinal(
+                  this.mfakey)));
+      
+      final TimeProvider timeProvider = new SystemTimeProvider();
+      final CodeGenerator codeGenerator = new DefaultCodeGenerator();
+      final CodeVerifier verifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+      
+      return verifier.isValidCode(mfakey, totp);
+    } catch(Exception e) {
+      throw new RuntimeException("Failed to verify TOTP", e);
+    }
+  }
+
+  /**
+   * Sets the user's MFA secret.
+   *
+   * @param mfakey the new MFA secret
+   * @return true if the new MFA key is different than the old one
+   */
+  public boolean setMFAKey(String mfakey) throws DecoderException {
+    byte[] prev = this.mfakey;
+    
+    if(null != mfakey) {
+      try {
+        final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+        var key = new SecretKeySpec(globalMFASecret, "AES");
+        
+        ByteBuffer idBuf = ByteBuffer.wrap(new byte[16]);
+        idBuf.putLong(id.getMostSignificantBits());
+        idBuf.putLong(id.getLeastSignificantBits());
+        var iv = new IvParameterSpec(idBuf.array());
+        
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        this.mfakey = cipher.doFinal(Base32.decode(mfakey));
+      } catch(DecoderException e) {
+        throw e;
+      } catch(Exception e) {
+        throw new RuntimeException("Failed to encrypt new MFA key", e);
+      }
+    } else this.mfakey = null;
+    
+    return null == prev && null != this.mfakey
+      || null != prev && null == this.mfakey
+      || !Arrays.equals(prev, this.mfakey);
+  }
+
+  /**
+   * Regenerates the user's MFA key.
+   *
+   * @return the string representation of the new MFA key
+   */
+  public String regenerateMFAKey() {
+    try {
+      final SecretGenerator secretGenerator = new DefaultSecretGenerator();
+      String mfakey = secretGenerator.generate();
+
+      final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+      var key = new SecretKeySpec(globalMFASecret, "AES");
+
+      ByteBuffer idBuf = ByteBuffer.wrap(new byte[16]);
+      idBuf.putLong(id.getMostSignificantBits());
+      idBuf.putLong(id.getLeastSignificantBits());
+      var iv = new IvParameterSpec(idBuf.array());
+
+      cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+      this.mfakey = cipher.doFinal(Base32.decode(mfakey));
+
+      return mfakey;
+    } catch(Exception e) {
+      throw new RuntimeException("Failed to regenerate MFA key", e);
+    }
+  }
+  
+}
---
+++ src/main/java/com/axonibyte/lib/auth/SessionEngine.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2022-2023 Axonibyte Innovations, LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.axonibyte.lib.auth;
+
+import java.nio.ByteBuffer;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.util.encoders.Base64;
+
+/**
+ * An engine that assists in the generation and verification of session keys.
+ *
+ * @author Caleb L. Power <cpower@axonibyte.com>
+ */
+public class SessionEngine {
+
+  private byte[] secret = new byte[32];
+  private int gracePeriod = 1;
+
+  /**
+   * Instantiates a new {@link SessionEngine}.
+   *
+   * @param secret the system secret used to encrypt session keys
+   * @param gracePeriod the number of minutes during which a session key remains valid
+   */
+  public SessionEngine(String secret, int gracePeriod) {
+    byte[] buf = secret.getBytes();
+    for(int i = 0; i < (this.secret.length > buf.length ? this.secret.length : buf.length); i++)
+      this.secret[i % this.secret.length] ^= buf[i % buf.length];
+    this.gracePeriod = gracePeriod;
+  }
+
+  /**
+   * Generates a session key for a particular user.
+   *
+   * @param user the {@link UUID} associated with the user in question
+   * @return some string that the user can use to maintain their session
+   */
+  public String generateSessionKey(UUID user) {
+    Objects.requireNonNull(user);
+    long now = System.currentTimeMillis() / 1000L;
+
+    try {
+      var key = new SecretKeySpec(this.secret, "AES");
+      var iv = new IvParameterSpec(toIV(now, gracePeriod));
+      Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+      cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+      ByteBuffer userBuf = ByteBuffer.wrap(new byte[16]);
+      userBuf.putLong(user.getMostSignificantBits());
+      userBuf.putLong(user.getLeastSignificantBits());
+
+      return new String(
+          Base64.encode(
+              cipher.doFinal(userBuf.array())));
+      
+    } catch(Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Verifies a particular session key.
+   *
+   * @param session the session key
+   * @return a {@link UUID} associated with a user, if the session was valid, or
+   *         {@code null} if the session could not be verified
+   */
+  public UUID verifySessionKey(String session) {
+    long candidate = System.currentTimeMillis() / 1000L;
+    UUID user = null;
+
+    try {
+      var key = new SecretKeySpec(secret, "AES");
+      Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+      byte[] ciphertext = Base64.decode(session.getBytes());
+      for(int i = 0; i < gracePeriod && null == user; i++) {
+        try {
+          var iv = new IvParameterSpec(toIV(candidate - i, gracePeriod));
+          cipher.init(Cipher.DECRYPT_MODE, key, iv);
+          ByteBuffer byteBuf = ByteBuffer.wrap(cipher.doFinal(ciphertext));
+          user = new UUID(byteBuf.getLong(), byteBuf.getLong());
+        } catch(Exception e) { }
+      }
+    } catch(Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    return user;
+  }
+
+  /**
+   * In short, this method is used to deterministically create a reproducable IV
+   * from some long value (presumably a timestamp) and some integer value
+   * (presumably the grace period). The signed long value `a` is encoded to the
+   * first 8 bytes of a 12-byte array, in order such that index 0 holds the most
+   * significant byte of `a` and index 7 holds the least significant byte of `a`.
+   * Then, each byte of the array undergoes an XOR operion with each byte of
+   * integer `b` in a reverse round-robin fashion. The resulting IV is sufficient
+   * to guarantee a unique ID for every combination of values for `a` and `b`.
+   */
+  private byte[] toIV(long a, int b) {
+    byte[] iv = new byte[12];
+    for(int i = 7; i >= 0; i--) {
+      iv[i] = (byte)(a & 0xFF);
+      a >>= 8;
+    }
+    for(int i = 11; i >= 0; i--)
+      iv[i] = (byte)(iv[i] ^ b >> i % 3 * 8);
+    return iv;
+  }
+
+}
